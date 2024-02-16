d9149b81b6a185e21687b1739d6617d0cb5af7c6
Infico <jangansampelupa200@gmail.com>
2024-02-17 05:57:26 +0900

    c5

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.write("Coding Here's our first attempt at using data to create a table:")
+st.write("Infinity Coding Club")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
