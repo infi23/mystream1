0e7c11b5afa75e9f5782bbef162e165aa851ed65
Infico <jangansampelupa200@gmail.com>
2024-02-17 12:32:21 +0900

    c8

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+st.write("Infinity Coding Club")
+# Membuat data dari dictionary
+# data = {
+#     'id': [1, 2, 3, 4, 5],
+#     'nama': ['Ali', 'Budi', 'Charlie', 'Dani', 'Eko'],
+#     'usia': [30, 25, 35, 40, 45],
+#     'pekerjaan': ['Dokter', 'Insinyur', 'Guru', 'Petani', 'Pengusaha']
+# }
+data = pd.read_csv('data.csv')
 
-st.write("Infinity Coding Club 2024")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+df = pd.DataFrame(data)
+
+# Menampilkan DataFrame
+st.write(df)
+
+# Membuat grafik batang dari kolom 'nama' dan 'usia'
+fig, ax = plt.subplots()
+ax.bar(df['nama'], df['usia'])
+plt.title('Usia berdasarkan Nama')
+plt.xlabel('Nama')
+plt.ylabel('Usia')
+
+# Menampilkan grafik
+st.pyplot(fig)
