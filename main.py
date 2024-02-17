25c022912281bbcba95962e6401ff8c48c87a9d4
Infico <jangansampelupa200@gmail.com>
2024-02-17 12:36:40 +0900

    gg

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 st.write("Infinity Coding Club")
 # Membuat data dari dictionary
-# data = {
-#     'id': [1, 2, 3, 4, 5],
-#     'nama': ['Ali', 'Budi', 'Charlie', 'Dani', 'Eko'],
-#     'usia': [30, 25, 35, 40, 45],
-#     'pekerjaan': ['Dokter', 'Insinyur', 'Guru', 'Petani', 'Pengusaha']
-# }
-data = pd.read_csv('data.csv')
+data = {
+    'id': [1, 2, 3, 4, 5],
+    'nama': ['Ali', 'Budi', 'Charlie', 'Dani', 'Eko'],
+    'usia': [30, 25, 35, 40, 45],
+    'pekerjaan': ['Dokter', 'Insinyur', 'Guru', 'Petani', 'Pengusaha']
+}
+#data = pd.read_csv('data.csv')
 
 df = pd.DataFrame(data)
 
